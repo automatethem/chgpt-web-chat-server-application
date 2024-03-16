7cd4aa2596bae19d38986430a6930c64332e35d5
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2024-03-16 21:23:39 +0900

    Update and rename AdminMenu.js to AdminMenu.js

--- app/admin/AdminMenus/chat/ai-web-chat-without-ai-rag/AdminMenu.js
+++ app/admin/AdminMenus/chat/ai-web-chat-without-ai-rag/AdminMenu.js
@@ -5,7 +5,7 @@ const Page = () => {
   <li>
     <h2 className="menu-title"><a href="/admin/chat/ai-web-chat">AI 웹 챗 관리</a></h2>
     <ul>
-        <li><a href="/chat/ai-web-chat" target="_blank">AI 웹 챗 가기</a></li>
+        <li><a href="/" target="_blank">AI 웹 챗 가기</a></li>
         <li><a href="/admin/chat/ai-web-chat/setting">설정</a></li>
         <li><a href="/admin/chat/ai-web-chat/connect">웹 사이트 연동 방법</a></li>
     </ul>
@@ -58,6 +58,7 @@ const Page = () => {
         <li><a href="/admin/server-management/supabase">수파베이스 데이터 베이스 관리 방법</a></li>
         <li><a href="/admin/server-management/aws">aws 서버 호스팅 관리 방법</a></li>
         <li><a href="/admin/server-management/cafe24">cafe24 서버 호스팅 관리 방법</a></li>
+        <li><a href="/admin/server-management/render">render 모델 호스팅 관리 방법</a></li>
         <li><a href="/admin/server-management/github">github 소스 코드 리포지토리 이전 받는 방법</a></li>
         <li><a href="/admin/server-management/vercel">vercel 웹 호스팅 관리 방법</a></li>
         <li><a href="/admin/server-management/github-access-token">github 액세스 토큰 만드는 방법</a></li>
