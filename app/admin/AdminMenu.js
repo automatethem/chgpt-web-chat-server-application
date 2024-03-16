1ec1ce31c0ec74c8cc559d7bf353bccc2db97b4e
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2024-03-16 21:25:59 +0900

    Update AdminMenu.js

--- app/admin/AdminMenu.js
+++ app/admin/AdminMenu.js
@@ -1,4 +1,3 @@
-//import Page from "./AdminMenus/chat/ai-web-chat-without-rag/custom/chgpt/AdminMenu.js";
 import Page from "./AdminMenus/chat/ai-web-chat-without-ai-rag/AdminMenu.js";
 
 export default Page;
