0b2ce93bdc7023e53ee189b2be081066c81516fa
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2024-03-20 14:03:04 +0900

    Add files via upload

--- app/api/chat/web-chat/langchain-agent-handler.js
+++ app/api/chat/web-chat/langchain-agent-handler.js
@@ -1,3 +1,3 @@
-import LangchainAgentHandler from "./langchain-agent-handler-v0.0.3.js";
+import LangchainAgentHandler from "./langchain-agent-handler-v0.0.6.js";
 
 export default LangchainAgentHandler;
