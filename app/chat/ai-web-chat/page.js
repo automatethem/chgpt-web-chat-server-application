f8b9cdac85826f4653c6081db9cc886c076e3a36
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2024-03-31 11:23:29 +0900

    Add files via upload

--- app/chat/ai-web-chat/page.js
+++ app/chat/ai-web-chat/page.js
@@ -41,6 +41,7 @@ function Page() {
   const messagesBottomRef = useRef(null);
   //
   const [menus, setMenus] = useState([]);
+  const [examples, setExamples] = useState([]);
   //
   const [id, setId] = useState(null);
   const [useLogoImage, setUseLogoImage] = useState(false);
@@ -49,6 +50,8 @@ function Page() {
   const [assistantImageUrl, setAssistantImageUrl] = useState('');
   const [useUserImage, setUseUserImage] = useState(false);
   const [userImageUrl, setUserImageUrl] = useState('');
+  const [useFooter, setUseFooter] = useState(false);
+  const [footer, setFooter] = useState('');
   const [useAiWebChat, setUseAiWebChat] = useState(true);
   const [loading, setLoading] = useState(true);
 
@@ -64,8 +67,20 @@ function Page() {
     else {
       console.error('Failed to fetch menu items:', error.message);
     }
-   
-    const { data: aiWebChatSetting, aiWebChatSettingError } = await supabase
+
+    const { data: aiWebChatExamples, error: aiWebChatExampleError } = await supabase
+      .from('AiWebChatExample')
+      .select('*')
+      .order("priority");
+    if (!error) {
+      setExamples(aiWebChatExamples);
+    } 
+    else {
+      console.error('Failed to fetch example items:', error.message);
+    }
+    console.log(aiWebChatExamples);
+
+    const { data: aiWebChatSetting, error: aiWebChatSettingError } = await supabase
     .from('AiWebChatSetting')
     .select('*')
     .single();
@@ -78,6 +93,8 @@ function Page() {
         assistantImageUrl,
         useUserImage,
         userImageUrl,
+        useFooter,
+        footer,
         useAiWebChat
       } = aiWebChatSetting;
       setId(id);
@@ -87,9 +104,12 @@ function Page() {
       setAssistantImageUrl(assistantImageUrl);
       setUseUserImage(useUserImage);
       setUserImageUrl(userImageUrl);
+      setUseFooter(useFooter);
+      setFooter(footer);
       setUseAiWebChat(useAiWebChat);
     }
     console.log(aiWebChatSetting);
+
     setLoading(false);
   }, []);
 
@@ -99,6 +119,14 @@ function Page() {
     }
   }, [messages]);
   
+  const handleExampleClick = (text) => {
+    handleInputChange({ target: { value: text } });
+
+    setTimeout(function(){
+      document.getElementsByTagName('button')[0].click();
+    }, 0.5);
+  };
+
   if (loading) 
     return <p>Loading...</p>;
 
@@ -106,6 +134,7 @@ function Page() {
     return <p>AI 웹 챗 미사용중</p>;
 
   return (
+    <>
     <section className='container mx-auto p-5 fixed inset-0'>
       <div className='fixed z-10 ml-4 mt-4'>
       {useLogoImage? <img src={logoImageUrl} className="max-w-sm rounded-lg shadow-2xl w-16" /> : <img src="/chat/ai-web-chat/logo.png" className="max-w-sm rounded-lg shadow-2xl w-16" />}
@@ -114,7 +143,14 @@ function Page() {
       <div className='fixed z-10 ml-20 mt-4'>
         <ul className="ml-4 menu menu-horizontal bg-base-200 rounded-box">
           {menus.map((menu) => {
-            return <li key={menu.id}><a href={menu.url} target="_blank">{menu.name}</a></li>
+            if (menu.url)
+              return <li key={menu.id} className="ml-1 mr-1"><a href={menu.url} target="_blank">{menu.name}</a></li>
+            else {
+              if (menus.length == 1)
+                return <li key={menu.id} className="ml-1 mr-1 font-bold">{menu.name}</li>
+              else
+                return <li key={menu.id} className="mt-2 ml-1 mr-1 font-bold">{menu.name}</li>
+            }
           })}
         </ul>
       </div>
@@ -173,20 +209,35 @@ function Page() {
           <div ref={messagesBottomRef}></div>
         </div>
 
-        <form className="form-control m-2 items-center" onSubmit={(e) => { setIsTyping(true); handleSubmit(e); }}>
+        <form className="form-control m-2 items-center" onSubmit={(e) => { setIsTyping(true); handleSubmit(e); document.getElementById('examples').style.display = "none"; }}>
           <div className="join w-full max-w-screen-md relative">
             {isTyping && <small className='absolute -top-5 left-0.5 animate-pulse'>AI가 메시지를 입력중 입니다...</small>}
-
             <input type="text" className="input w-full input-bordered join-item flex-grow" placeholder="메시지를 입력하세요..." value={input} onChange={handleInputChange} required/>
             <button className="btn btn-square" type="submit" disabled={isLoading}>
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="currentColor" viewBox="0 0 16 16">
-                <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15" />
               </svg>
             </button>
           </div>
+
+          <div className="w-full max-w-screen-md relative" id="examples">
+            {examples.map((example) => {
+              return <button className="btn btn-sm btn-outline mr-1 mt-1" key={example.id} onClick={(e) => {handleExampleClick(example.name); }}>{example.name}</button>
+            })}
+          </div>
         </form>
       </div>
+
+      {useFooter ? 
+        <div className="flex w-full justify-center">
+        <p className="text-xs">{footer}</p>
+        </div>
+        : null
+      }
     </section>
+
+
+    </>
   );
 }
 
