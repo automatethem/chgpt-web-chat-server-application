e45feac91733f1efccd9d261701296c3bee8d807
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2024-04-17 08:53:00 +0900

    Delete app/api/chat/web-chat directory

--- app/api/chat/web-chat/langchain-agent-tools/supabase-retriever-tool.js
+++
@@ -1,3 +0,0 @@
-import supabaseRetrieverTool from "./supabase-retriever-tool-v0.0.5.js"
-
-export default supabaseRetrieverTool;
--- app/api/chat/web-chat/langchain-agent-tools/tavilysearch-tool.js
+++
@@ -1,3 +0,0 @@
-import createTavilysearchTool from "./tavilysearch-tool-v0.0.3.js"
-
-export default createTavilysearchTool;
