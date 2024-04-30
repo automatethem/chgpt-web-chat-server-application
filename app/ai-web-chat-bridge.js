9e196c37fcf4587dcb3b946aa2ed95217f5a44c1
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2024-05-01 01:13:22 +0900

    Add files via upload

---
+++ app/ai-web-chat-bridge.js
@@ -0,0 +1,12 @@
+///*
+export default function AiWebChat() {
+  return <div>이 기능을 추가하려면 개발자에게 문의 하세요.</div>;
+}
+//*/
+/*
+import AiWebChat from "./chat/ai-web-chat/page.js";
+export default function AiWebChat() {
+  ////const AiWebChat = require("./chat/ai-web-chat/page.js").default;
+  return <AiWebChat/>;
+}
+*/
