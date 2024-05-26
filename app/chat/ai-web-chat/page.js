08b97f15e599866e0374c1f3265cfaae402564f8
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2024-05-26 19:50:01 +0900

    Update page.js

--- app/chat/ai-web-chat/page.js
+++ app/chat/ai-web-chat/page.js
@@ -125,7 +125,7 @@ function Page() {
     handleInputChange({ target: { value: text } });
 
     setTimeout(function(){
-      //document.getElementsByTagName('button')[0].click();
+      document.getElementById('submitButton').click();
     }, 0.5);
   };
 
@@ -199,6 +199,9 @@ function Page() {
                         a: ({ node, ...props }) => (
                           <a {...props} className="underline" />
                         ),
+                        button: ({ node, ...props }) => {
+                          return <button className="btn btn-sm mr-1 mt-1" onClick={(e) => {handleExampleClick(props.children[0]); }}>{props.children[0]}</button>
+                        },
                       }}
                     >
                       {msg.content}
@@ -215,7 +218,7 @@ function Page() {
           <div className="join w-full max-w-screen-md relative">
             {isTyping && <small className='absolute -top-5 left-0.5 animate-pulse'>AI가 메시지를 입력중 입니다...</small>}
             <input type="text" className="input w-full input-bordered join-item flex-grow" placeholder="메시지를 입력하세요..." value={input} onChange={handleInputChange} required/>
-            <button className="btn btn-square" type="submit" disabled={isLoading}>
+            <button className="btn btn-square" type="submit" id="submitButton" disabled={isLoading}>
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15" />
               </svg>
